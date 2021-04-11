343f3fb0d0274903a8601bd64f835c9377d0d644
Dusan <podman.dusan@gmail.com>
2021-04-11 14:49:24 +0200

    Changed pane layout

---
+++ src/controller/CreateDayTasks.java
@@ -0,0 +1,23 @@
+//package controller;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.TableColumn;
+//import javafx.scene.control.TableView;
+//
+//import java.time;
+//
+//public class CreateDayTasks {
+//
+//    @FXML TableView<Task> tableTasks;
+//    @FXML TableColumn<Task, Time> columnStart;
+//    @FXML TableColumn<Task, Time> columnEnd;
+//    @FXML TableColumn<Task, String> columnTask;
+//
+//    public void btnAddOnAction(){
+//
+//    }
+//
+//    public void btnDeleteOnAction(){
+//
+//    }
+//}
